63e7ec820879fa75e7b2910fcda9da65a9c79a53
zxb <614619114@qq.com>
2019-03-26 14:46:02 +0800

    修正一个小错误，该错误下className不可用

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './importIcons'
 import './icon.scss'
 
-interface IconProps extends React.DOMAttributes<SVGElement> {
+interface IconProps extends React.SVGAttributes<SVGElement> {
     name: string;
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
+    const {className, ...restProps} = props;
     return (
-        <svg className='zui-icon' {...props}>
+        <svg className={`zui-icon ${className}`} {...restProps}>
             <use xlinkHref={`#${props.name}`}/>
         </svg>
     );
