b7c325ee7c064c0ebc1685af9be294b8c3adf69d
zxb <614619114@qq.com>
2019-03-28 09:31:53 +0800

    icon点击事件测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,17 @@
 import Icon from "../icon";
 import * as renderer from 'react-test-renderer';
 import React from 'react';
+import {mount} from 'enzyme'
 
 describe('icon', () => {
-    it('icon xxx', () => {
-        const json = renderer.create(<Icon />).toJSON();
+    it('render icon successful', () => {
+        const json = renderer.create(<Icon name='alipay' />).toJSON();
         expect(json).toMatchSnapshot();
     });
+    it('onClick function',()=>{
+        const fn = jest.fn();
+        const component = mount(<Icon name='alipay' onClick={fn}/>);
+        component.find('svg').simulate('click');
+        expect(fn).toBeCalled();
+    })
 });
