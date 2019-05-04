f91c47dadcf37fb4ffd4d7bcb13ac7acfcca45ad
想飞天的招雄彬 <614619114@qq.com>
2019-05-04 19:36:56 +0800

    再重构 scopedClassMaker

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -13,28 +13,16 @@ interface ClassToggles {
 }
 
 function scopedClassMaker(prefix: string) {
-    return function (name: string | ClassToggles, options?: Options) {
-        // name = {hasAside:true ,active:false,x:true,y:false}
-        const namesObject = (typeof name === 'string' || name === undefined) ?
-            {[name]: name}:
-            name
-        ;
-
-        const scoped = Object
-            .entries(namesObject)//拿到namesObject所有键值对
+    return (name: string | ClassToggles, options?: Options) =>
+         Object
+            .entries(name instanceof Object ? name : {[name]: name})//拿到namesObject所有键值对
             .filter(kv => kv[1] !== false) //找出所有value不为false的键值对
             .map(kv => kv[0])//取到相应的key
-            .map(name =>[prefix, name]
+            .map(name => [prefix, name]
                 .filter(Boolean)
-                .join("-"))
+                .join("-")) // 'zui-layout zui-layout-hasAside'
+            .concat(options && options.extra || [])
             .join(' ');//对这些key加前缀的操作，不需要对空的项加，最后再用空格连起来
-
-        if (options && options.extra) {
-            return [scoped, options && options.extra].filter(Boolean).join(' ')
-        } else {
-            return scoped
-        }
-    };
 }
 
 export {scopedClassMaker};
