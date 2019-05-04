5c83fda5dd8883ae99aecfd0e49178c685e5251f
想飞天的招雄彬 <614619114@qq.com>
2019-05-04 19:39:35 +0800

    稍微再重构 scopedClassMaker

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -12,17 +12,17 @@ interface ClassToggles {
     [K: string]: boolean;
 }
 
-function scopedClassMaker(prefix: string) {
-    return (name: string | ClassToggles, options?: Options) =>
-         Object
-            .entries(name instanceof Object ? name : {[name]: name})//拿到namesObject所有键值对
-            .filter(kv => kv[1] !== false) //找出所有value不为false的键值对
-            .map(kv => kv[0])//取到相应的key
-            .map(name => [prefix, name]
-                .filter(Boolean)
-                .join("-")) // 'zui-layout zui-layout-hasAside'
-            .concat(options && options.extra || [])
-            .join(' ');//对这些key加前缀的操作，不需要对空的项加，最后再用空格连起来
-}
+const scopedClassMaker = (prefix: string) =>
+    (name: string | ClassToggles, options?: Options) =>
+     Object
+        .entries(name instanceof Object ? name : {[name]: name})//拿到namesObject所有键值对
+        .filter(kv => kv[1] !== false) //找出所有value不为false的键值对
+        .map(kv => kv[0])//取到相应的key
+        .map(name => [prefix, name]
+            .filter(Boolean)
+            .join("-")) // 'zui-layout zui-layout-hasAside'
+        .concat(options && options.extra || [])
+        .join(' ');//对这些key加前缀的操作，不需要对空的项加，最后再用空格连起来
+//这代码是方方大佬的写法，妈的，看不懂
 
 export {scopedClassMaker};
